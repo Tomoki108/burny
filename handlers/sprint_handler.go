3f5fc3847f5d58efc45f0fe6ca9759dc19962714
Tomoki108 <shinonome81@gmail.com>
2024-06-13 22:32:05 +0900

    add handlers

---
+++ handlers/sprint_handler.go
@@ -0,0 +1,72 @@
+package handlers
+
+import (
+	"burny-api/db"
+	"burny-api/models"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type Sprint struct {
+	gorm.Model
+	ProjectID  uint               `json:"project_id" gorm:"index"`
+	Name       string             `json:"name"`
+	StartDate  time.Time          `json:"start_date"`
+	EndDate    time.Time          `json:"end_date"`
+	SprintStat *models.SprintStat `json:"sprint_stat" gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+func CreateSprintHandler(c echo.Context) error {
+	sprint := new(Sprint)
+	if err := c.Bind(sprint); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	if err := db.DB.Create(sprint).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusCreated, sprint)
+}
+
+func GetSprintHandler(c echo.Context) error {
+	db := c.Get("db").(*gorm.DB)
+	id := c.Param("id")
+	var sprint Sprint
+	if err := db.Preload("SprintStat").First(&sprint, id).Error; err != nil {
+		return c.JSON(http.StatusNotFound, err)
+	}
+	return c.JSON(http.StatusOK, sprint)
+}
+
+func UpdateSprintHandler(c echo.Context) error {
+	id := c.Param("id")
+	var sprint Sprint
+	if err := db.DB.First(&sprint, id).Error; err != nil {
+		return c.JSON(http.StatusNotFound, err)
+	}
+	if err := c.Bind(&sprint); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	if err := db.DB.Save(&sprint).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, sprint)
+}
+
+func DeleteSprintHandler(c echo.Context) error {
+	id := c.Param("id")
+	if err := db.DB.Delete(&Sprint{}, id).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.NoContent(http.StatusNoContent)
+}
+
+func ListSprintsHandler(c echo.Context) error {
+	var sprints []Sprint
+	if err := db.DB.Preload("SprintStat").Find(&sprints).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, sprints)
+}
