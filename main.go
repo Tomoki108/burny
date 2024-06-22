2784a766cc2aef7815e280104d3e4c1643c6900d
Tomoki108 <shinonome81@gmail.com>
2024-06-22 22:18:34 +0900

    add cors setting, test api

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type data struct {
+	Date        string `json:"date"`
+	SolarPanels int    `json:"SolarPanels"`
+	Inverters   int    `json:"Inverters"`
+}
+
 func main() {
 	// Echoのインスタンス作成
 	e := echo.New()
@@ -35,7 +41,24 @@ func main() {
 	e.GET("/projects/:id", handlers.GetProjectHandler)
 	e.PUT("/projects/:id", handlers.UpdateProjectHandler)
 	e.DELETE("/projects/:id", handlers.DeleteProjectHandler)
+	e.GET("test", func(c echo.Context) error {
+		list := []data{
+			{Date: "Jan 22", SolarPanels: 2890, Inverters: 2338},
+			{Date: "Feb 22", SolarPanels: 2756, Inverters: 2103},
+			{Date: "Mar 22", SolarPanels: 3322, Inverters: 2194},
+			{Date: "Apr 22", SolarPanels: 3470, Inverters: 2108},
+			{Date: "May 22", SolarPanels: 3475, Inverters: 1812},
+			{Date: "Jun 22", SolarPanels: 3129, Inverters: 1726},
+			{Date: "Jul 22", SolarPanels: 3490, Inverters: 1982},
+			{Date: "Aug 22", SolarPanels: 2903, Inverters: 2012},
+		}
 
+		return c.JSON(http.StatusOK, list)
+	})
+
+	// CORSの設定。localhost（別のポート）からの通信を許可するだけならこれで十分らしい。
+	// https://qiita.com/sola-msr/items/828e2eb45cf05b1a2ad4
+	e.Use(middleware.CORS())
 	// サーバーの開始
 	e.Logger.Fatal(e.Start(":1323"))
 }
