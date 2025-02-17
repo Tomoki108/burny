2e2e7008cdb64c937d21f3331a1d064c8083cb1e
Tomoki108 <shinonome81@gmail.com>
2025-02-17 16:39:21 +0900

    wip e2e

--- api/e2e/e2e_test.go
+++ api/e2e/e2e_test.go
@@ -1,13 +1,16 @@
 package e2e
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	"github.com/Tomoki108/burny/config"
 	"github.com/Tomoki108/burny/di"
 	"github.com/Tomoki108/burny/handler"
+	"github.com/Tomoki108/burny/infrastructure"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,19 +18,28 @@ import (
 func TestE2E(t *testing.T) {
 	e := echo.New()
 
-	req := httptest.NewRequest(http.MethodPost, "/sign_up", strings.NewReader(`{"email":"test@test.com","password":"passwd"}`))
+	req := httptest.NewRequest(http.MethodPost, "/sign_up", strings.NewReader(`{"email":"test@test.com","password":"passwd12345"}`))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	recorder := httptest.NewRecorder()
 
 	c := e.NewContext(req, recorder)
 
+	if err := config.Init(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// DB接続
+	if err := infrastructure.ConnectDB(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	di.ProvideDependencies()
 	var authH handler.AuthHandler
 	di.Container.Invoke(func(h handler.AuthHandler) {
 		authH = h
 	})
-	h := http.HandlerFunc(authH.SignUp)
 
-	if assert.NoError(t, h.SignUp(c)) {
+	if assert.NoError(t, authH.SignUp(c)) {
 		// レスポンスの検証
 		assert.Equal(t, http.StatusOK, recorder.Code)
 		// その他のアサーション（例: レスポンスボディの検証）を実施
--- api/go.mod
+++ api/go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/labstack/echo-jwt/v4 v4.3.0
 	github.com/labstack/echo/v4 v4.13.3
+	github.com/stretchr/testify v1.10.0
 	github.com/swaggo/echo-swagger v1.4.1
 	github.com/swaggo/swag v1.16.4
 	go.uber.org/dig v1.18.0
@@ -20,6 +21,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
@@ -38,6 +40,7 @@ require (
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/swaggo/files/v2 v2.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
