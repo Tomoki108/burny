6cfd4304d57607d9e011668d9923a10cc5b3bb0d
Tomoki108 <shinonome81@gmail.com>
2025-02-18 21:14:43 +0900

    add postgres resource

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -9,3 +9,24 @@ resource "google_artifact_registry_repository" "cloud_run_repo" {
   description   = "Artifact Registry repository for Cloud Run application images"
   format        = "DOCKER"
 }
+
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = "dev-postgres-instance"
+  database_version = "POSTGRES_13"
+  region           = "asia-northeast1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "default" {
+  name     = "burny_db"
+  instance = google_sql_database_instance.postgres_instance.name
+}
+
+resource "google_sql_user" "default" {
+  name     = "burny_user"
+  instance = google_sql_database_instance.postgres_instance.name
+  password = var.postgres_password
+}
--- infra/dev/variables.tf
+++ infra/dev/variables.tf
@@ -2,3 +2,8 @@ variable "project_id" {
   description = "Google Cloud Project ID"
   type        = string
 }
+
+variable "postgres_password" {
+  description = "Cloud SQL PostgreSQL password"
+  type        = string
+}
