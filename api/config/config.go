b9aa8dcb87afc711db1c7fc2df9af7042ba35bb1
Tomoki108 <shinonome81@gmail.com>
2025-02-21 22:32:37 +0900

    mod conf

--- api/config/config.go
+++ api/config/config.go
@@ -2,14 +2,15 @@ package config
 
 import "github.com/caarlos0/env/v11"
 
+// NOTE: envDefault is used for local environment and ci workflow environment.
 type Config struct {
-	JwtSecret                   string `env:"JWT_SECRET"`
-	Host                        string `env:"HOST"`
-	Port                        string `env:"PORT"`
-	DB_NAME                     string `env:"DB_NAME"`
-	DB_USER                     string `env:"DB_USER"`
-	DB_PASS                     string `env:"DB_PASS"`
-	DB_INSTANCE_CONNECTION_NAME string `env:"DB_INSTANCE_CONNECTION_NAME"`
+	JwtSecret                   string `env:"JWT_SECRET" envDefault:"random-str"`
+	Host                        string `env:"HOST" envDefault:"localhost"`
+	Port                        string `env:"PORT" envDefault:"1323"`
+	DB_NAME                     string `env:"DB_NAME" envDefault:"burny_db"`
+	DB_USER                     string `env:"DB_USER" envDefault:"burny_user"`
+	DB_PASS                     string `env:"DB_PASS" envDefault:"pass"`
+	DB_INSTANCE_CONNECTION_NAME string `env:"DB_INSTANCE_CONNECTION_NAME" envDefault:""`
 }
 
 var Conf Config
