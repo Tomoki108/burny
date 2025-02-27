2721c6b8f7cce8954efe874853f888724d49274e
Tomoki108 <shinonome81@gmail.com>
2025-02-27 14:57:08 +0900

    install vuetify

---
+++ web_vue/src/router/index.ts
@@ -0,0 +1,23 @@
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
