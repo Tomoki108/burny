c375cbd58c5a5d51b7da520758f3755e858a518b
Tomoki108 <shinonome81@gmail.com>
2025-03-18 14:54:51 +0900

    add api mock

--- web/tests/mock_server.ts
+++ web/tests/mock_server.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse, passthrough } from "msw";
 import { setupWorker } from "msw/browser";
+import type { Project } from "../src/api/project_api";
 
 const API_HOST = "http://localhost:1323/api/v1";
 
@@ -26,6 +27,27 @@ const handlers = [
   }),
   http.get(`${API_HOST}/projects`, () => {
     return HttpResponse.json([], { status: 200 });
+  }),
+  http.post(`${API_HOST}/projects`, async (request) => {
+    const json = await request.request.json();
+    const pj = json?.valueOf();
+    const project = pj as Project;
+
+    return HttpResponse.json(
+      {
+        id: 1,
+        user_id: project.user_id,
+        title: project.title,
+        sprint_count: project.sprint_count,
+        description: project.description,
+        sprint_duration: project.sprint_duration,
+        start_date: project.start_date,
+        total_sp: project.total_sp,
+        created_at: "2024-01-01T00:00:00Z",
+        updated_at: "2024-01-01T00:00:00Z",
+      },
+      { status: 201 }
+    );
   }),
   // CSS, Vue, TypeScriptファイルを取得するリクエストをそのまま通す（unhandle requestの警告を消すため）
   http.get(new RegExp("\\.(css|vue|ts)$"), () => {
