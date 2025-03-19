0c832158ed91df86dcc01a573c187cba8eb94595
Tomoki108 <shinonome81@gmail.com>
2025-03-19 16:33:12 +0900

    mod

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 import { useAuthStore } from "./stores/auth_store";
+import Home from "./views/Home.vue";
+import Account from "./views/Account.vue";
+import Projects from "./views/Projects.vue";
+import ProjectDetail from "./views/ProjectDetail.vue";
 
 export const PATH_HOME = "/";
 export const PATH_ACCOUNT = "/account";
@@ -12,24 +16,24 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: PATH_HOME,
     name: "Home",
-    component: () => import("./views/Home.vue"),
+    component: Home,
   },
   // auth required routes
   {
     path: PATH_ACCOUNT,
     name: "Account",
-    component: () => import("./views/Account.vue"),
+    component: Account,
   },
   {
     path: PATH_PROJECTS,
     name: "Projects",
-    component: () => import("./views/Projects.vue"),
+    component: Projects,
     meta: { requiresAuth: true },
   },
   {
     path: PATH_PROJECT_DETAIL,
     name: "ProjectDetail",
-    component: () => import("./views/ProjectDetail.vue"),
+    component: ProjectDetail,
     meta: { requiresAuth: true },
   },
 ];
