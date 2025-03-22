ae4915a135133fd21fdfb6b593d766e59c208275
Tomoki108 <shinonome81@gmail.com>
2025-03-22 11:56:07 +0900

    mock design

--- web/src/views/Account.vue
+++ web/src/views/Account.vue
@@ -1,7 +1,20 @@
 <template>
     <ContentsContainer title="Account">
         <v-card title="Email" :text=authStore.email class="mb-5"></v-card>
-        <v-card title="Password" text="********"></v-card>
+        <v-card title="Password" text="********" class="mb-5"></v-card>
+        <v-card title="API Key" text="*******" class="mb-5">
+            <v-card-actions>
+                <button data-testid="edit-project-button" class="button-small bg-color-muted">
+                    Create
+                </button>
+                <button data-testid="delete-project-button" class="button-small-danger">
+                    Delete
+                </button>
+            </v-card-actions>
+        </v-card>
+        <v-alert type="info" class="mt-3">API Document: http://localhost:1323/swagger/index.html</v-alert>
+
+
     </ContentsContainer>
 </template>
 
