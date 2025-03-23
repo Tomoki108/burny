c6658f18e542cd18f862e6bfa73ec56650633e21
Tomoki108 <shinonome81@gmail.com>
2025-03-23 19:30:43 +0900

    mod mail

--- api/domain/mail.go
+++ api/domain/mail.go
@@ -1,5 +1,7 @@
 package domain
 
+import "github.com/Tomoki108/burny/config"
+
 type Mail struct {
 	To      string
 	Subject string
@@ -7,11 +9,12 @@ type Mail struct {
 }
 
 func NewEmailVerificationMail(to, token string) Mail {
+	url := config.Conf.Host + "/api/v1/verify_email?token=" + token
 	return Mail{
 		To:      to,
 		Subject: "Burny Email Verification",
-		Body: "Please verify your email by clicking the link in 20 minitues: https://example.com/verify?token=" + token +
-			"\n\nIf you did not request this, please ignore this email.",
+		Body: "Please verify your email by clicking the link in 20 minitues: " + url +
+			"\nIf you did not request this, please ignore this email.",
 	}
 }
 
